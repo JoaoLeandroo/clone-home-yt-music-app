f12a04e4d0ba430cdc8f49030ff0c7cef8238e2a
joaoleandro <new.joaoleandro@gmail.com>
2023-08-28 19:52:50 -0300

    layout header, pronto

--- src/app/components/Header/page.jsx
+++ src/app/components/Header/page.jsx
@@ -2,13 +2,14 @@ import { SiYoutubemusic } from 'react-icons/si'
 import { PiListBold } from 'react-icons/pi'
 import { BsSearch } from 'react-icons/bs'
 import { LuMonitorSmartphone } from 'react-icons/lu'
+import { BiSolidUserCircle } from 'react-icons/bi'
 
 export default function Header() {
     return(
         <header className='w-full h-16 flex justify-between p-3 border-b border-solid border-[#757575]'>
             <div className='w-[10%] flex gap-3 justify-center items-center'>
-                <div className='cursor-pointer'>
-                    <PiListBold size={20} color='white'/>
+                <div className='cursor-pointer ml-2'>
+                    <PiListBold size={30} color='white'/>
                 </div>
                 <div className='flex cursor-pointer'>
                     <SiYoutubemusic size={30} color='red'/>
@@ -32,9 +33,9 @@ export default function Header() {
 
             </div>
 
-            <div className='w-[10%]'>
-                <LuMonitorSmartphone size={20}/>
-                <h2>ME</h2>
+            <div className='w-[10%] f-full flex items-center justify-center gap-5'>
+                <LuMonitorSmartphone size={25} color='white'/>
+                <BiSolidUserCircle size={30} color='white'/>
             </div>
         </header>
     )
